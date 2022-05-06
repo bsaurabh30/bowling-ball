6e3b5e1298c82285833da0694764063a1df90ae6
bsaurabh30 <b.saurabh30@gmail.com>
2022-05-06 12:14:29 +0530

    Add project files.

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            var game = new Game();
+            Roll(game, 0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [TestMethod]
+        public void CanInstanciateGameObjectTest()
+        {
+            new Game();
+        }
+
+        [TestMethod]
+        public void CanRollTheBallTest()
+        {
+            var game = new Game();
+            game.Roll(0);
+        }
+
+        [TestMethod]
+        public void GetTheScoreTest()
+        {
+            var game = new Game();
+            for (int i = 0; i < 20; i++)
+                game.Roll(1);
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        [TestMethod]
+        public void ThrowingASpareTest()
+        {
+            var game = new Game();
+            game.Throws(new[] { 5, 5, 8, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            Assert.AreEqual(32, game.GetScore());
+
+
+        }
+        [TestMethod]
+        public void StrikeTest()
+        {
+            var game = new Game();
+            game.Throws(new[] { 10, 8, 8, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            Assert.AreEqual(48, game.GetScore());
+
+        }
+        [TestMethod]
+        public void PerfectGameTesst()
+        {
+            var game = new Game();
+            game.Throws(new[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+            Assert.AreEqual(300, game.GetScore());
+        }
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,46 @@
+﻿namespace BowlingBall
+{
+    public class Game
+    {
+        public int[] Frames = new int[21];
+        public int chance = 0;
+        public void Throws(int[] frame)
+        {
+            for (int i = 0; i < frame.Length; i++)
+            {
+                Roll(frame[i]);
+            }
+        }
+        public void Roll(int pins)
+        {
+            // Add your logic here. Add classes as needed.
+            Frames[chance++] = pins;
+        }
+
+        public int GetScore()
+        {
+            // Returns the final score of the game.
+            int score = 0;
+            int j = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (Frames[j] == 10)
+                {
+                    score += Frames[j] + Frames[j + 1] + Frames[j + 2];
+                    j++;
+                }
+                else if (Frames[j] + Frames[j + 1] == 10)
+                {
+                    score += Frames[j] + Frames[j + 1] + Frames[j + 2];
+                    j = j + 2;
+                }
+                else
+                {
+                    score += Frames[j] + Frames[j + 1];
+                    j += 2;
+                }
+            }
+            return score;
+        }
+    }
+}
